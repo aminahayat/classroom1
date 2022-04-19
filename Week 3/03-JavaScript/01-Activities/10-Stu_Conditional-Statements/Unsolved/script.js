6f098618df1c97e723ea3944dc5545751124cfdf
Amina Hayat <hayat.amina@hotmail.com>
2022-04-19 19:05:20 +0100

    git commit

--- Week 3/03-JavaScript/01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
+++ Week 3/03-JavaScript/01-Activities/10-Stu_Conditional-Statements/Unsolved/script.js
@@ -18,7 +18,44 @@
 //     console.log(number)
 //     number = number + 1
 // }
+//  >>>>>>>> THE FOLLOWING IS HOW TO WRITE IT IN A SHORTER WAY <<<<<<<<<<<
 
-for (let number = 0; number < 100; number = number + 1) {
-    console.log('new version:  ' + number)
+// for (let number = 0; number < 100; number = number + 1) {
+//     console.log('new version:  ' + number)
+// }
+
+// for (let number = 20; number < 60; number = number + 1) {
+//     console.log('new version:   ' + number)
+// }
+
+// >>>>>>>>> WRITING EVEN/ODD NUMBERS <<<<<<<<<
+// EVEN NUMBERS 
+
+// for (let i = 0; i < 1000; i = i + 1) {
+//     if (i % 2 === 0) {
+//         console.log(i)
+//     }
+// }
+
+//  for (let i = 0; i < 1000; i = i + 2) {
+//      console.log(i)
+    
+//  }
+
+// ODD NUMBERS
+
+// for (let i = 0; i < 1000; i = i + 1) {
+//     if (i % 1 === 0) {
+//         console.log(i)
+//         }
+//     } 
+
+// for (let i = 0; i < 1000; i = i + 1) {
+//     console.log(i)
+// }
+
+let array = [2, 4, 6, 8, 10]
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i])
 }
