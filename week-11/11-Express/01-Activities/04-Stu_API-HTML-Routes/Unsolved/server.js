5a842e9be7609f2c519852da5f2afc2b58006f3b
Amina Hayat <hayat.amina@hotmail.com>
2022-06-14 20:06:16 +0100

    git commit new work

--- week-11/11-Express/01-Activities/04-Stu_API-HTML-Routes/Unsolved/server.js
+++ week-11/11-Express/01-Activities/04-Stu_API-HTML-Routes/Unsolved/server.js
@@ -1,11 +1,16 @@
 // TODO: Import express
+const express = require("express")
+const termData = require("./terms.json");
 
 // TODO: Import 'terms.json' file
 const PORT = 3001;
 
+const app = express();
+
 // TODO: Initialize app variable
 
 // TODO: Create a route for a GET request that will return the content of our `terms.json` file
+
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
