c6f2f5eaa88fdd2cd7a218cc01590b76828e473a
Amina Hayat <hayat.amina@hotmail.com>
2022-06-14 19:55:48 +0100

    git commit

--- week-10/10-OOP/01-Activities/01-Activities/24-Stu_Subclasses/Unsolved/car.js
+++ week-10/10-OOP/01-Activities/01-Activities/24-Stu_Subclasses/Unsolved/car.js
@@ -1,6 +1,16 @@
 // TODO: Import the parent class
+const vehicle = require('./vehicle');
 
 // TODO: Create a `Car` class that extends the `Vehicle` class
+class car extends vehicle {
+  constructor(id, color, Passengers) {
+    super(id, 4, 'beep');
+
+    this.color = color;
+    this.Passengers = Passengers;
+  }
+}
+
 
 const carPassengers = [
   'Aristotle',
@@ -11,8 +21,8 @@ const carPassengers = [
 ];
 
 const car = new Car(15, 'blue', carPassengers);
-
-console.log('---CAR---');
-car.printInfo();
-car.useHorn();
-car.checkPassengers();
+console.log(car)
+// console.log('---CAR---');
+// car.printInfo();
+// car.useHorn();
+// car.checkPassengers();
---
+++ week-11/11-Express/05-Notes/session-1/03-frontend.md
@@ -0,0 +1,13 @@
+# Using your API from the frontend
+
+## Demo
+
+* Open [07-Ins_GET-Fetch/server.js](../activities/07-Ins_GET-Fetch/server.js)
+  * Demonstrate how the frontend calls the server
+* Open [09-Ins_Static-Assets/server_static.js](../activities/09-Ins_Static-Assets/server_static.js)
+
+## Try
+
+* Try [08-Stu_GET-Fetch/README.md](../activities/08-Stu_GET-Fetch/README.md)
+* Try [10-Stu_Static-Assets/README.md](../activities/10-Stu_Static-Assets/README.md)
+* Please install [insomnia](https://insomnia.rest/download)
